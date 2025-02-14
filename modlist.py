a137d4d25dc8daa32e1df6a792bd19dd250bbf4b
Thomas Jenkins <117096929+thomasjenkins27@users.noreply.github.com>
2025-02-14 15:51:11 -0600

    Update modlist.py
    Cleaner code by defining functions.

--- modlist.py
+++ modlist.py
@@ -1,12 +1,17 @@
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        unprinted_designs.sort()
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design.title()}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    print("The following models have been completed: ")
+    for complete_models in completed_models:
+        print(complete_models.title())
+
 unprinted_designs = ['model plane', 'robot', 'phone case']
 completed_models= []
 
-while unprinted_designs:
-    unprinted_designs.sort()
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
-
-print("The following models have been completed: ")
-for complete_models in completed_models:
-    print(complete_models.title())
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)

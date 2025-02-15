a82e6288cd34b4c29d3bd6b7c2b0c88c282bc96f
Thomas Jenkins <117096929+thomasjenkins27@users.noreply.github.com>
2025-02-15 13:17:05 -0600

    Update modlist.py
    Updated and cleaner version of original code.

--- modlist.py
+++ modlist.py
@@ -1,17 +1,25 @@
-def print_models(unprinted_designs, completed_models):
-    while unprinted_designs:
-        unprinted_designs.sort()
-        current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design.title()}")
-        completed_models.append(current_design)
+#Goal is to create 2 lists called unprinted_list and printed_list.  
+#Create 2 funtions. Modify/copy 1 list into the next.
+def incomplete_tasks(unprinted_list, printed_list):
+#Sort list to show in alphabetical order.
+    unprinted_list.sort(reverse=True)
+    while unprinted_list:
+        current_task = unprinted_list.pop()
+        print(f"Currently printing: {current_task.title()}")
+        printed_list.append(current_task)
 
-def show_completed_models(completed_models):
-    print("The following models have been completed: ")
-    for complete_models in completed_models:
-        print(complete_models.title())
+def show_completed_task(printed_list):
+    print("\nThe following items have been printed: ")
+    for completed_list in printed_list:
+        print(completed_list.title())
 
-unprinted_designs = ['model plane', 'robot', 'phone case']
-completed_models= []
+unprinted_list = ['airplane', 'quadcopter', 'dog']
+printed_list = []
 
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
+#Make a copy of the list to keep the original list
+incomplete_tasks(unprinted_list[:], printed_list)
+show_completed_task(printed_list)
+
+#Proof that the original list is left unchanged.
+print("\nThis is the original list: ")
+print(unprinted_list)
